5a849e36b62270a6c635d33ca24f9b1f8de71be2
pixeldewads-digital <pixeldew.ads@gmail.com>
2026-01-01 07:04:01 +0800

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,8 +9,17 @@ export default function Logo() {
       className="flex items-center"
     >
       <div
-        className="relative h-7 w-7 sm:h-8 sm:w-8 lg:h-10 lg:w-10"
-      >
+className="
+  relative
+  h-7 w-7
+  sm:h-8 sm:w-8
+  lg:h-10 lg:w-10
+  transition-transform duration-200
+  hover:scale-105
+  focus-visible:outline-none
+  focus-visible:ring-2
+  focus-visible:ring-orange-500
+"      >
        <Image
           src="/logo/dew-flow-logo.svg"
           alt="Dew Flow Planner"
