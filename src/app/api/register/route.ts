909b3319823b481500d1f7fad584f3329f05fe3e
pixeldewads-digital <pixeldew.ads@gmail.com>
2026-01-01 13:24:27 +0800

    Change Prisma import source in route.ts

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,7 +1,7 @@
 
 import { NextResponse } from 'next/server';
 import { hash } from 'bcrypt';
-import { Prisma } from "@prisma/client";
+import { Prisma } from 'prisma/schema.prisma';
 
 export async function POST(req: Request) {
   try {
