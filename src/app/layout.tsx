a6272aabf452e29e098c86082f7e7fec9a8278fb
pixeldewads-digital <pixeldew.ads@gmail.com>
2026-01-02 03:50:45 +0800

    Import Header component in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Header from "@/components/Header/Header";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
